125264391dc7a1e1a38914ee689da1d938bc593c
hangrybear666 <hangrybear666+github@proton.me>
2025-12-13 23:08:46 +0100

    Remove ephemeral inet access after haproxy oci pull

--- scripts/deploy_latest_haproxy.sh
+++ scripts/deploy_latest_haproxy.sh
@@ -3,17 +3,17 @@
 scp .env Dockerfile haproxy.cfg ./errorfiles/503.http root@loadbalancer:/usr/local/etc/haproxy/
 
 # giving loadbalancer temporary internet access until reboot for pulling haproxy image
-# ssh loadbalancer << 'EOF'
-# timeout 2 curl -s ifconfig.me
-# exit_code=$?
-# # only execute if curl times out
-# if (( $exit_code > 0 )); then
-#   printf "\nSetting up ephemeral internet access via NAT GW\n"
-#   /root/scripts/nat_gw_ephemeral_public_egress.sh 172.24.1.3
-# else
-#   printf "\nInternet Access already setup. Continue.\n"
-# fi
-# EOF
+ssh loadbalancer << 'EOF'
+timeout 2 curl -s ifconfig.me
+exit_code=$?
+# only execute if curl times out
+if (( $exit_code > 0 )); then
+  printf "\nSetting up ephemeral internet access via NAT GW\n"
+  /root/scripts/nat_gw_ephemeral_public_egress.sh 172.24.1.3
+else
+  printf "\nInternet Access already setup. Continue.\n"
+fi
+EOF
 
 ssh loadbalancer << EOF
   cd /usr/local/etc/haproxy/
@@ -26,6 +26,9 @@ ssh loadbalancer << EOF
   podman container rm haproxy > /dev/null 2>&1
 
   # sudo sysctl net.ipv4.ip_unprivileged_port_start=80
+
+  # remove default rule pointing to nat gateway for internet access to reenable private network connections
+  ip rule delete from all lookup nat_prod
 
   podman run \
     --name haproxy \
